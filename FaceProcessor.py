b739306a57d3138466b459b2ca25184997eb562d
rajur <rajur@thoughtworks.com>
2016-10-06 16:28:13 +0530

    [#Refactoring][Raju] modified some methods in HTTPServer and moved some methods from HTTPServer file to FaceProcessor file

---
+++ FaceProcessor.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+import cv2
+import numpy as np
+
+from FaceRecognizer import Recognizer, FaceRecognizerWindow
+
+from PyQt4 import QtGui
+
+file = 'learnedData.yml'
+faceRecognizer = Recognizer(file)
+
+
+def recognizeFace(predictedFace):
+    predictedFace.value = None
+    photos = readFrames("Test", 4, 2)
+    print photos
+    if len(photos) != 0:
+        predicted = faceRecognizer.predict(photos)
+        data = Counter(predicted)
+        print "Predicted", data.most_common(1)[0][0]
+        predictedFace.value = data.most_common(1)[0][0]
+    cv2.destroyAllWindows()
+    return predictedFace
+
+
+def trainForFace(label):
+    photos = readFrames("Train", 8, 2)
+    labels = [label for i in range(len(photos))]
+    faceRecognizer.update(photos, np.array(labels))
+    cv2.destroyAllWindows()
+    print "captured and trained for => " + str(labels)
+
+
+def readFrames(Message, numberOfFrames, delay):
+    app = QtGui.QApplication([Message])
+    photos = []
+    w = FaceRecognizerWindow(photos, numberOfFrames, delay, None)
+    w.setWindowTitle('Face Recognizer')
+    w.show()
+    app.exec_()
+    return photos
--- HttpServer.py
+++ HttpServer.py
@@ -1,25 +1,17 @@
-from collections import Counter
-
-import cv2
-import numpy as np
 from bottle import route, run, hook, response, request
+from FaceProcessor import recognizeFace, trainForFace
+from multiprocessing import Process, Manager
 
 from DB import DB
 import sqlite3
-from FaceRecognizer import Recognizer, FaceRecognizerWindow
-
-from PyQt4 import QtGui
-from multiprocessing import Process, Manager
 
 host = "10.136.22.28"  # Change this to machine IP
 port = 8080
-
+db = DB()
 
 @hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
-db = DB()
-
 
 @route('/train')
 def train():
@@ -34,9 +26,6 @@ def train():
         return "Trained for " + uuid + " =>" + str(mappedId)
     except sqlite3.IntegrityError:
         print "User with uuid=%s already exists,"  # initiating re-training."
-        mappedId = db.getId(uuid)
-        # p = Process(target=trainForFace, args=(mappedId,))
-        # p.start()
         return "I Know You"
 
 
@@ -48,64 +37,13 @@ def predict():
     p.join()
 
     predictedId = predicted.value
-    result = getUUID(predictedId)
+    result = db.getUuid(predictedId)
 
     print 'result', result
     if result is not None:
         returnString = "You are %s" % (result[0],)
         return returnString
     return "Unable to Recognize You"
-
-
-def getUUID(predictedId):
-    result = db.getUuid(predictedId)
-    return result
-
-
-file = 'learnedData.yml'
-faceRecognizer = Recognizer(file)
-
-
-def recognizeFace(predictedFace):
-    predictedFace.value = None
-    app = QtGui.QApplication(["Test"])
-    photos = []
-    w = FaceRecognizerWindow(photos, 4, 2, None)
-    w.setWindowTitle('Face Recognizer')
-    w.show()
-    app.exec_()
-
-    print photos
-    if len(photos) != 0:
-        predicted = faceRecognizer.predict(photos)
-        data = Counter(predicted)
-        print "Predicted", data.most_common(1)[0][0]
-        predictedFace.value = data.most_common(1)[0][0]
-    cv2.destroyAllWindows()
-    return predictedFace
-
-
-def trainForFace(label):
-    app = QtGui.QApplication(["Train"])
-    photos = []
-    w = FaceRecognizerWindow(photos, 8, 2, None)
-    w.setWindowTitle('Face Recognizer')
-    w.show()
-    app.exec_()
-    # For face recognition we will the the LBPH Face Recognizer
-    # label = int(label)
-    labels = [label for i in range(len(photos))]
-
-    # cv2.namedWindow('face')
-    # for faces in photos:
-    #     cv2.imshow('face', faces)
-    #     cv2.waitKey(60)
-    #     time.sleep(1)
-    # Train
-    faceRecognizer.update(photos, np.array(labels))
-    cv2.destroyAllWindows()
-    print "captured and trained for => " + str(labels)
-
 
 if __name__ == '__main__':
     run(host=host, port=port, debug=True)
