0b6c9a79827c879ecbdcb62f6fe02aca037f010a
N.Sai Kiran <nsaikiran@thoughtworks.com>
2016-10-10 11:44:13 +0530

    [Refactor][Sai Kiran/Raju] Rename & extraction of methods

--- TemperatureSensor.py
+++ TemperatureSensor.py
@@ -8,35 +8,15 @@ class TemperatureSensor:
         self.__serialInput = serialInput
         self.__thresholdValue = 90
 
-    def setTemperature(self):
+    def read(self):
         while True:
             temperature = self.getAbsoluteTemperatureFromSerial()
             self.__isValid = temperature > self.__thresholdValue
             if self.__isValid:
-                self.addTemperature()
-                temperature = self.getAbsoluteTemperatureFromSerial()
-                while temperature > self.__thresholdValue:
-                    self.consoleOutput("Resetting sensor temperature, current=>%.2f" % temperature)
-                    time.sleep(3)
-                    temperature = self.getAbsoluteTemperatureFromSerial()
-
+                self.update()
+                self.reset()
             else:
                 self.consoleOutput("value=%.2fF smaller than threshold." % temperature)
-
-    def addTemperature(self):
-        self.consoleOutput("calculating avg temp =>")
-        averageTemperature = self.getAverageTemperatureValue()
-        self.consoleOutput(averageTemperature)
-        recordsUpdater = RecordsUpdater()
-        recordsUpdater.updateLatestRecords('Temperature', averageTemperature)
-        self.__isValid = False
-        time.sleep(2)
-
-    def getAverageTemperatureValue(self):
-        readings = []
-        for i in range(10):
-            readings.append(self.getAbsoluteTemperatureFromSerial())
-        return round(reduce(lambda x, y: x + y, readings) / float(len(readings)), 2)
 
     def getAbsoluteTemperatureFromSerial(self):
         temperature = re.findall("\d+\.\d+", self.__serialInput.readline())
@@ -44,8 +24,30 @@ class TemperatureSensor:
             return float(temperature[0])
         return 0
 
+    def update(self):
+        self.consoleOutput("calculating avg temp =>")
+        averageTemperature = self.getAverageValue()
+        self.consoleOutput(averageTemperature)
+        recordsUpdater = RecordsUpdater()
+        recordsUpdater.updateLatestRecords('Temperature', averageTemperature)
+        self.__isValid = False
+        time.sleep(3)
+
+    def getAverageValue(self):
+        readings = []
+        for i in range(10):
+            readings.append(self.getAbsoluteTemperatureFromSerial())
+        return round(reduce(lambda x, y: x + y, readings) / float(len(readings)), 2)
+
+    def reset(self):
+        temperature = self.getAbsoluteTemperatureFromSerial()
+        while temperature > self.__thresholdValue:
+            self.consoleOutput("Resetting sensor temperature, current=>%.2f" % temperature)
+            time.sleep(3)
+            temperature = self.getAbsoluteTemperatureFromSerial()
+
     def consoleOutput(self, message):
         print message
 
 temperatureSensor = TemperatureSensor(serial.Serial("/dev/cu.usbmodem1421", 9600))
-temperatureSensor.setTemperature()
+temperatureSensor.read()
