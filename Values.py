7fd44a96dcf0257ac5e3bd8358967b22f91d883e
N.Sai Kiran <nsaikiran@thoughtworks.com>
2016-10-12 12:10:54 +0530

    [Refactor][Sai Kiran] Adds haarcascade_frontalface_default.xml to project

--- Values.py
+++ Values.py
@@ -4,7 +4,7 @@ HOST_PORT = 8080
 
 DATABASE_FILE = "FaceRecognizer.db"
 LEARNED_DATA_FILE = "LearnedData.yml"
-FRONTAL_FACE_XML_FILE = "/usr/local/Cellar/opencv3/3.1.0_4/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
+FRONTAL_FACE_XML_FILE = "haarcascade_frontalface_default.xml"
 
 SERIAL_PORT = "/dev/cu.usbmodem1421"
 BAUD_RATE = 9600
